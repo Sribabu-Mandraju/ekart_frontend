9ca932585060f9a5e671cb7a5f9d82580d5a1e73
Sireesha <n210413@rguktn.ac.in>
2024-10-26 10:28:26 -0700

    ho

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Team from "./pages/team/Team";
 
 const App = () => {
   return (
     <>
+      <Team />
       <div className="text-center text-2xl">HELLO</div>
     </>
   );
 };
 
 export default App;
+
---
+++ src/pages/team/Team.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FaLinkedin } from 'react-icons/fa';
+
+function Team() {
+  const profiles = [
+    { name: "Sireesha", profession: "Software Engineer", linkdin: "https://www.linkedin.com/in/johndoe/" },
+    { name: "Sribabu", profession: "Data Scientist", linkdin: "https://www.linkedin.com/in/johndoe/" },
+    { name: "Nakshakthra", profession: "UX Designer", linkdin: "https://www.linkedin.com/in/johndoe/" },
+    { name: "Bindu", profession: "Product Manager", linkdin: "https://www.linkedin.com/in/johndoe/" },
+    { name: "Dharaneswar", profession: "DevOps Engineer", linkdin: "https://www.linkedin.com/in/johndoe/" },
+  ];
+
+  return (
+    <div className="flex flex-wrap justify-center">
+      {profiles.map(profile => (
+        <div key={profile.name} className="h-[300px] w-[300px] border-3 shadow-lg m-4 flex items-center justify-center">
+          <div className="flex items-center justify-center flex-col gap-2 mt-[150px] hover:bg-[#A0A0A0] h-[150px] w-full">
+            <div className="text-black text-2xl">{profile.name}</div>
+            <div className="text-[#333333]">{profile.profession}</div>
+            <a href={profile.linkdin} target="_blank" rel="noopener noreferrer">
+              <FaLinkedin size={25} className="text-blue-700" />
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Team;
